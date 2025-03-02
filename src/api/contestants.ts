6cd03141a52e0fd8d72f6e647e4d45520798baec
Sebastian Andrade <sandradecedano@gmail.com>
2025-03-02 17:29:17 -0500

    API obtains the looged in handle from req, updated those requests

--- src/api/contestants.ts
+++ src/api/contestants.ts
@@ -9,22 +9,20 @@ export const getSubmissionActivity = async (handle: string, from: string, to: st
   })
 };
 
-export const getContestantsForFriendsPage = async (pageLen: number, page: number, user: string, filter: ("all" | "friends")) => {
+export const getContestantsForFriendsPage = async (pageLen: number, page: number,  filter: ("all" | "friends")) => {
   return instance.get<{numOfPages:number, contestants: Array<ContestantInfoRow>}>(`/contestants`, {
     params: {
         pageLen:pageLen,
-        user:user,
         page:page,
         filter:filter,
     }
   })
 };
 
-export const searchContestantsForFriendsPage = async (pageLen: number, page: number, user: string, handle: string) => {
+export const searchContestantsForFriendsPage = async (pageLen: number, page: number, handle: string) => {
   return instance.get<{numOfPages:number, contestants: Array<ContestantInfoRow>}>(`/contestants/search`, {
     params: {
         pageLen:pageLen,
-        user:user,
         page:page,
         handle:handle,
     }
