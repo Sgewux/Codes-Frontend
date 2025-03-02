8457a33785f4277cab44011f29438df5137aa0d0
cmottao <cristianstevenmottaojeda@gmail.com>
2025-03-02 13:16:52 -0500

    Connected submit page to backend

--- src/api/submissions.ts
+++ src/api/submissions.ts
@@ -3,3 +3,7 @@ import instance from "./axios";
 export const getSubmission = async (id: number) => {
   return instance.get<SubmissionDetail>(`/submissions/${id}`);
 };
+
+export const postSubmission = async (problem_id: number, code: string) => {
+  return instance.post("/submissions", { problem_id, code });
+};
